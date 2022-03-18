a0899f05930d6dcdb8ca31191e50ad9f358b0989
ipekozbay <20060401@stu.omu.edu.tr>
2022-03-18 23:57:38 +0300

    18.03.2022

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,48 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+data = {
+    "blogs":[
+        {
+            "id": 1,
+            "title" : "js",
+            "image": "js.jpeg",
+            "is_active": True,
+            "is_home": False,
 
+            "description": "good course"
+        },
+                {
+            "id": 2,
+            "title" : "django",
+            "image": "django2.jpeg",
+            "is_active": True,
+            "is_home": True,
+
+            "description": "good course"
+        },
+                {
+            "id": 3,
+            "title" : "web",
+            "image": "web.jpeg",
+            "is_active": False,
+            "is_home": True,
+
+            "description": "good course"
+        },
+    ]
+}
 def index(request):
-    return render(request,"blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request,"blog/index.html",context)
 
 def blogs(request):
-    return render(request,"blog/blogs.html")  
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request,"blog/blogs.html",context)  
      
 def blog_details(request,id):
     return render(request,"blog/blog-details.html",{
