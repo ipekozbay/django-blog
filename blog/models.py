333b44d4b9ccb17eba760ff22a151eda9a7988ad
ipekozbay <20060401@stu.omu.edu.tr>
2022-03-21 17:58:01 +0300

    created models. 21.03.2022

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from email.mime import image
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title =models.CharField(max_length=200)
+    image = models.CharField(max_length=50)
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
